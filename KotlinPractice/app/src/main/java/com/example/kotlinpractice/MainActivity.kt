1114f372029db9e1930f23e164e4c3c3c390ae61
raae7742 <aeae_1@naver.com>
2021-08-01 23:07:47 +0900

    심리테스트 앱: UI 디자인

--- KotlinPractice/app/src/main/java/com/example/kotlinpractice/MainActivity.kt
+++ KotlinPractice/app/src/main/java/com/example/kotlinpractice/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.kotlinpractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
